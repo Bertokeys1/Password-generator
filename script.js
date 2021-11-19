e1ee0d80a1dc31ca52f2e5e52b244315b03bb625
Bertokeys1 <abarrientos93.ab@gmail.com>
2021-11-18 18:56:52 -0800

    updated character legnth prompts to stay within requirements using  and greater/less than symbols for if/if else statements

--- script.js
+++ script.js
@@ -5,23 +5,30 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numeric = [0,1,2,3,4,5,6,7,8,9];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "=", "+", "-", "/", ".", "?", "<", ">"];
 
+var numPrompt = false;
+var lwrPrompt = false;
+var uprPrompt = false;
+var spclPrompt = false;
 
 function generatePassword(){
   var pwLength = prompt( " Please select passwork length (between 8-128 characters) ")
   var pwChrctrs = [];
   var pwResult = [];
 
-  if(pwLength > 8) {
-    prompt = (" Password needs to be greater than 8 characters. ")
-    return;
+  if(pwLength < 8 ) {
+    prompt (" Password needs to be greater than 8 characters. ");
+    return "";
   } 
-  else if(pwLength < 128) {
-    prompt = (" Password needs to be lower than 128 characters. ")
+  else if(pwLength > 128 ) {
+    prompt (" Password needs to be lower than 128 characters. ");
+    return "";
   }
-  var numPrompt = confirm(" Would you like to include numbers? ")
-  var lwrPrompt = confirm(" would you like to include lowercase letters? ")
-  var uprPrompt = confirm(" Would you like to inlude uppercase letters? ")
-  var spclPrompt = confirm("would you like to include special characters? ")
+  numPrompt = confirm(" Would you like to include numbers? ")
+  lwrPrompt = confirm(" would you like to include lowercase letters? ")
+  uprPrompt = confirm(" Would you like to inlude uppercase letters? ")
+  spclPrompt = confirm("would you like to include special characters? ")
+
+  if ( !numPrompt && !lwrPrompt && !uprPrompt && !spclPrompt)
 }
 
 
@@ -29,6 +36,8 @@ function generatePassword(){
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
