6e21ca75335ffa7a6c6776a03d480270b9a06af0
Bertokeys1 <abarrientos93.ab@gmail.com>
2021-11-20 16:11:35 -0800

    added variable prompts and modified if statement with ! and &&.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Password-generator
 homwork3 JS password generator 
+
+This application will prompt requirements to generate a random password. When running the application, you will be asked if you want lower and uppercase letters for your password, then you will be asked if you want to included numbers and special characters. After the promps, you will have a random password in the length you choose and all requirements in propmts. 
+
+-git hub repo <https://github.com/Bertokeys1/Password-generator>
+-live page <https://bertokeys1.github.io/Password-generator/>
+
--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+var numeric = [0,1,2,3,4,5,6,7,8,9];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numeric = [0,1,2,3,4,5,6,7,8,9];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "=", "+", "-", "/", ".", "?", "<", ">"];
 
-var numPrompt = false;
-var lwrPrompt = false;
-var uprPrompt = false;
-var spclPrompt = false;
 
 function generatePassword(){
-  var pwLength = prompt( " Please select password length (between 8-128 characters) ")
-  var pwChrctrs = [];
-  var password = [];
+  var pwLength = prompt( " Please select password length between 8-128 characters. ")
+  var pwChrctrs = []
+  var password = []
 
   if(pwLength < 8 ) {
     prompt (" Password needs to be greater than 8 characters. ");
@@ -23,20 +20,17 @@ function generatePassword(){
     prompt (" Password needs to be lower than 128 characters. ");
     return "";
   } else {
-    numPrompt = confirm(" Would you like to include numbers? ")
-    lwrPrompt = confirm(" would you like to include lowercase letters? ")
-    uprPrompt = confirm(" Would you like to inlude uppercase letters? ")
-    spclPrompt = confirm("would you like to include special characters? ")
+    var numPrompt = confirm(" Would you like to include numbers? ")
+    var lwrPrompt = confirm(" would you like to include lowercase letters? ")
+    var uprPrompt = confirm(" Would you like to inlude uppercase letters? ")
+    var spclPrompt = confirm("would you like to include special characters? ")
 
-
-    if (!uprPrompt &&  !lwrPrompt && !numPrompt && !spclPrompt) {
-      alert("Please choose at least one option.");
+      // 
+    if (!numPrompt && !lwrPrompt && !uprPrompt && !spclPrompt) {
+      prompt("Please choose an option.");
       return "";
-
-}
-  
-  
-  
+    }
+      
 
 
 
